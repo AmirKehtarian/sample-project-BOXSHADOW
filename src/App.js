2e1b5dc43e6831995f81c331a4d0c0d415c9bfc7
AmirKehtarian <a.kehtarian@gmail.com>
2022-10-11 23:02:18 +0200

    edit my website

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Mainpage />} />
         <Route path='/Sample-Project-todolist' element={<ToDoList />} />
+        <Route path='https://amirkehtarian.github.io/Sample-Project-todolist' element={<ToDoList />} />
         <Route path='/Sample-Project-boxshadow' element={<BoxShadow />} />
+        <Route path='https://amirkehtarian.github.io/Sample-Project-boxshadow' element={<BoxShadow />} />
         <Route path='/mySampleWebsite' element={<App/>} />
         <Route path='http://AmirKehtarian.github.io/mySampleWebsite' element={<App/>} />
       </Routes>
